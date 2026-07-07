969a7455d93c9bfd78ff43161b74d199f840b854
aa048193679-star <aa048193679@gmail.com>
2026-07-07 15:28:11 +0700

    Lesson2

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+# 23/6/69
 print("Hello, World!")
+
+# 30/6/69
+print("Information and Network Engineering")
+print("Hello Phornphirom you will be success")
+print("I'm hungry")
+print('He ask me "Who got the book?"')
+print("I'm good and \"I will do my best\"")
+print('/\\')
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
+print('100.00')
+print('%d' %100)
+print('%d' %100.58)
+print('%f' %-100.89)
+print('%.2f' %100.89)
+
+from math import pi
+print('%.2f' %pi)
+print('%.4f' %pi)
+print('%.50f' %pi)
+
+print('My age is', 25,'I have', 3500.50, 'Bth.')
+print('My age is %d I have %.2f Bth.' %(25,3500.50))
+print('My age is ' + str(25) + ' I have ' + str(3500.50) + ' Bth.')
+print( '5+4 =' ,5+4)
+print( '5+4 = %.2f' % (5+4))
+print( '5+4 = ' +str(5+4))
+
+print("I'm Phornphirom", "I'll keep practicing!")
+print("I'm Phornphirom " + "I'll keep practicing!")
+
+print(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+print('%d %d %d %d %d %d %d %d %d %d' %(1,1,2,3,5,8,13,21,34,55))
+
+# 7/7/69
